adbd979a877d6f9ceed3594bac7ab2d748b82ad7
Matthias Mees <mm@yellowled.de>
2014-09-22 14:05:17 +0200

    Adapt main template file to my boilerplate.

--- _main.php
+++ _main.php
@@ -26,70 +26,13 @@
     <script src="<?php echo $config->urls->templates?>scripts/modernizr/modernizr.js"></script>
 </head>
 <body>
-	<!-- top navigation -->
-	<ul class='topnav'>
-		<?php
-		// top navigation consists of homepage and its visible children
-		foreach($homepage->and($homepage->children) as $item) {
-			if($item->id == $page->rootParent->id) echo "<li class='current'>";
-				else echo "<li>";
-			echo "<a href='$item->url'>$item->title</a></li>";
-		}
+    <main>
+        <article>
+            <h2><?php echo $title; ?></h2>
 
-		// output an "Edit" link if this page happens to be editable by the current user
-		if($page->editable()) echo "<li class='edit'><a href='$page->editURL'>Edit</a></li>";
-		?>
-	</ul>
-
-	<form class='search' action='<?php echo $pages->get('template=search')->url; ?>' method='get'>
-		<input type='text' name='q' placeholder='Search' value='<?php echo $sanitizer->entities($input->whitelist('q')); ?>' />
-		<button type='submit' name='submit'>Search</button>
-	</form>
-
-	<!-- breadcrumbs -->
-	<div class='breadcrumbs'>
-		<?php
-		// breadcrumbs are the current page's parents
-		foreach($page->parents() as $item) {
-			echo "<span><a href='$item->url'>$item->title</a></span> ";
-		}
-		// optionally output the current page as the last item
-		echo "<span>$page->title</span> ";
-		?>
-	</div>
-
-	<div id='main'>
-
-		<!-- main content -->
-		<div id='content'>
-			<h1><?php echo $title; ?></h1>
-			<?php echo $content; ?>
-		</div>
-
-		<!-- sidebar content -->
-		<?php if($sidebar): ?>
-		<div id='sidebar'>
-			<?php echo $sidebar; ?>
-		</div>
-		<?php endif; ?>
-
-	</div>
-
-	<!-- footer -->
-	<footer id='footer'>
-		<p>
-		Powered by <a href='http://processwire.com'>ProcessWire CMS</a>  &nbsp; / &nbsp;
-		<?php
-		if($user->isLoggedin()) {
-			// if user is logged in, show a logout link
-			echo "<a href='{$config->urls->admin}login/logout/'>Logout ($user->name)</a>";
-		} else {
-			// if user not logged in, show a login link
-			echo "<a href='{$config->urls->admin}'>Admin Login</a>";
-		}
-		?>
-		</p>
-	</footer>
+            <?php echo $content; ?>
+        </article>
+    </main>
 
 <!--[if lt IE 8]> <p id="old-ie" class="chromeframe">Ihr Browser ist veraltet! <a href="http://browsehappy.com/">Verwenden Sie einen anderen Browser</a> oder <a href="http://www.google.com/chromeframe/?redirect=true">installieren Sie Google Chrome Frame</a>, damit diese Website korrekt angezeigt wird.</p> <![endif]-->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
