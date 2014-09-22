d25c2e57455f0aaefedbfa98a01615ee6f3a787b
Matthias Mees <mm@yellowled.de>
2014-09-22 12:12:04 +0200

    Adapt new PW 2.5 files to my template.

--- _init.php
+++ _init.php
@@ -3,28 +3,19 @@
 /**
  * Initialize variables output in _main.php
  *
- * Values populated to these may be changed as desired by each template file.
- * You can setup as many such variables as you'd like. 
- *
  * This file is automatically prepended to all template files as a result of:
- * $config->prependTemplateFile = '_init.php'; in /site/config.php. 
- *
- * If you want to disable this automatic inclusion for any given template, 
- * go in your admin to Setup > Templates > [some-template] and click on the 
- * "Files" tab. Check the box to "Disable automatic prepend file". 
- *
+ * $config->prependTemplateFile = '_init.php'; in /site/config.php.
  */
 
-// Variables for regions we will populate in _main.php
-// Here we also assign default values for each of them.
-$title = $page->get('headline|title'); 
-$content = $page->body;
-$sidebar = $page->sidebar;
+// Helper variables
+$homepage = $pages->get('/');
 
-// We refer to our homepage a few times in our site, so 
-// we preload a copy here in $homepage for convenience. 
-$homepage = $pages->get('/'); 
+// Meta data
+$title = $page->get('headline|title');
+$description = $page->summary;
+
+// Content regions
+$content = $page->body;
 
 // Include shared functions
-include_once("./_func.php"); 
-
+include_once("./_func.php");
--- _main.php
+++ _main.php
@@ -1,46 +1,34 @@
-<?php 
+<?php
 
 /**
  * _main.php
- * Main markup file
  *
- * This file contains all the main markup for the site and outputs the regions 
- * defined in the initialization (_init.php) file. These regions include: 
- * 
- *   $title: The page title/headline 
- *   $content: The markup that appears in the main content/body copy column
- *   $sidebar: The markup that appears in the sidebar column
- * 
- * Of course, you can add as many regions as you like, or choose not to use
- * them at all! This _init.php > [template].php > _main.php scheme is just
- * the methodology we chose to use in this particular site profile, and as you
- * dig deeper, you'll find many others ways to do the same thing. 
- * 
- * This file is automatically appended to all template files as a result of 
- * $config->appendTemplateFile = '_main.php'; in /site/config.php. 
- *
- * In any given template file, if you do not want this main markup file 
- * included, go in your admin to Setup > Templates > [some-template] > and 
- * click on the "Files" tab. Check the box to "Disable automatic append of
- * file _main.php". You would do this if you wanted to echo markup directly 
- * from your template file or if you were using a template file for some other
- * kind of output like an RSS feed or sitemap.xml, for example. 
+ * This file is automatically appended to all template files as a result of
+ * $config->appendTemplateFile = '_main.php'; in /site/config.php.
  *
  */
-?><!DOCTYPE html>
-<html lang="en">
+?><!doctype html>
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="de"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="de"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?php echo $title; ?></title>
-	<meta name="description" content="<?php echo $page->summary; ?>" />
-	<link href='http://fonts.googleapis.com/css?family=Lusitana:400,700|Quattrocento:400,700' rel='stylesheet' type='text/css' />
-	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+	<meta name="description" content="<?php echo $description; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="dns-prefetch" href="//ajax.googleapis.com">
+<!--[if lte IE 8]>
+    <link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/oldie.css">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/master.css">
+<!--<![endif]-->
+    <script src="<?php echo $config->urls->templates?>scripts/modernizr/modernizr.js"></script>
 </head>
-<body class="<?php if($sidebar) echo "has-sidebar "; ?>">
-
+<body>
 	<!-- top navigation -->
 	<ul class='topnav'>
-		<?php 
+		<?php
 		// top navigation consists of homepage and its visible children
 		foreach($homepage->and($homepage->children) as $item) {
 			if($item->id == $page->rootParent->id) echo "<li class='current'>";
@@ -60,13 +48,13 @@
 
 	<!-- breadcrumbs -->
 	<div class='breadcrumbs'>
-		<?php 
+		<?php
 		// breadcrumbs are the current page's parents
 		foreach($page->parents() as $item) {
-			echo "<span><a href='$item->url'>$item->title</a></span> "; 
+			echo "<span><a href='$item->url'>$item->title</a></span> ";
 		}
 		// optionally output the current page as the last item
-		echo "<span>$page->title</span> "; 
+		echo "<span>$page->title</span> ";
 		?>
 	</div>
 
@@ -90,8 +78,8 @@
 	<!-- footer -->
 	<footer id='footer'>
 		<p>
-		Powered by <a href='http://processwire.com'>ProcessWire CMS</a>  &nbsp; / &nbsp; 
-		<?php 
+		Powered by <a href='http://processwire.com'>ProcessWire CMS</a>  &nbsp; / &nbsp;
+		<?php
 		if($user->isLoggedin()) {
 			// if user is logged in, show a logout link
 			echo "<a href='{$config->urls->admin}login/logout/'>Logout ($user->name)</a>";
@@ -103,5 +91,9 @@
 		</p>
 	</footer>
 
+<!--[if lt IE 8]> <p id="old-ie" class="chromeframe">Ihr Browser ist veraltet! <a href="http://browsehappy.com/">Verwenden Sie einen anderen Browser</a> oder <a href="http://www.google.com/chromeframe/?redirect=true">installieren Sie Google Chrome Frame</a>, damit diese Website korrekt angezeigt wird.</p> <![endif]-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo $config->urls->templates?>scripts/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="<?php echo $config->urls->templates?>scripts/master.js"></script>
 </body>
 </html>
