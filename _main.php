5cf19b8746685af53b2e1297e5c0f1a0806b1437
Matthias Mees <mm@yellowled.de>
2015-01-03 14:06:41 +0100

    Update jQuery CDN references to latest.

--- _main.php
+++ _main.php
@@ -36,7 +36,7 @@
         </article>
     </main>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php echo $config->urls->templates?>scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="<?php echo $config->urls->templates?>scripts/master.js"></script>
 </body>
