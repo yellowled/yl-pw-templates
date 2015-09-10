cf4fc8a864007e43d9c33b0098eace6cb50c4b56
Matthias Mees <mm@yellowled.de>
2015-09-10 12:05:41 +0200

    Adapt to recent changes in my project boilerplate

--- _main.php
+++ _main.php
@@ -7,36 +7,33 @@
  * $config->appendTemplateFile = '_main.php'; in /site/config.php.
  *
  */
-?><!doctype html>
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="de"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="de"> <![endif]-->
+?><!DOCTYPE html>
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="de"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?></title>
     <meta name="description" content="<?php echo $description; ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php echo renderCanonical($canonicalURL); ?>
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="dns-prefetch" href="//ajax.googleapis.com">
-<!--[if lte IE 8]>
-    <link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/oldie.css">
-<![endif]-->
-<!--[if gt IE 8]><!-->
     <link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/master.css">
-<!--<![endif]-->
     <script src="<?php echo $config->urls->templates?>scripts/modernizr/modernizr.js"></script>
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 <body>
     <main>
         <article>
-            <h2><?php echo $headline; ?></h2>
+            <h1><?php echo $headline; ?></h1>
 
             <?php echo $content; ?>
         </article>
     </main>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php echo $config->urls->templates?>scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="<?php echo $config->urls->templates?>scripts/master.js"></script>
 </body>
