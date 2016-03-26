cd131bce17507440862f9aa7815aac9e45640ac1
Matthias Mees <mm@yellowled.de>
2016-03-26 10:20:43 +0100

    Remove arrow chars from prev/next links

--- gallery-page.php
+++ gallery-page.php
@@ -9,8 +9,8 @@ $thumb_per_page = 9;
 $thumb_width    = 200;
 $thumb_height   = 150;
 // Pager labels
-$pager_next     = 'Weiter »';
-$pager_prev     = '« Zurück';
+$pager_next     = 'Weiter';
+$pager_prev     = 'Zurück';
 
 $gallery_start = ($input->pageNum - 1) * $thumb_per_page;
 $gallery_total = count($page->images);
