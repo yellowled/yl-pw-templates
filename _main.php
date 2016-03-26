e9c3b5bd402e2c541cbb644f9233d997136fded9
Matthias Mees <mm@yellowled.de>
2016-03-26 10:18:54 +0100

    Use jQuery CDN

--- _main.php
+++ _main.php
@@ -29,7 +29,7 @@
         </article>
     </main>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php echo $config->urls->templates?>scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="<?php echo $config->urls->templates?>scripts/master.js"></script>
 </body>
