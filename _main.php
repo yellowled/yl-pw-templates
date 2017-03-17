4a12c45c60d6075bb9862daec76b9a8be9430f38
Matthias Mees <mm@yellowled.de>
2017-03-17 08:32:40 +0100

    Upgrade jQuery to 3.2.0

--- _main.php
+++ _main.php
@@ -29,7 +29,7 @@
         </article>
     </main>
 
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php echo $config->urls->templates?>scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="<?php echo $config->urls->templates?>scripts/master.js"></script>
 </body>
